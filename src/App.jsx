7fe9020e6789e1cb25b4c1871652ab64aa2c93ef
TareqHasan18 <akmhasan18@gmail.com>
2025-01-03 12:15:11 -0500

    lesson for single state variable

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import Fruits from './components/Fruits';
 import Hello from './components/Hello';
 import ConditionalComponent from './components/ConditionalComponent';
 import Message from './components/Message';
+import Counter from './components/Counter';
 
 function App() {
   return (
     <div className="App">
-      <Message />
+      <Counter />
     </div>
   );
 }
